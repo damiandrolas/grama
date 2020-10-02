8e52c326f5291c1f00f72e4906e9e6b7ba78e160
Damian Drolas <37936021+damiandrolas@users.noreply.github.com>
2020-10-02 11:12:22 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request,redirect, flash
 from forms import contactForm ## instancia de la clase creada en forms.py
 from flask_mail import Mail, Message  # 1. Importamos la clase Mail y message
+import os
 
 app = Flask(__name__)
 
@@ -10,14 +11,14 @@ app.secret_key = 'clavesecreta'
 
 #app.config['DEBUG'] = True
 app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
-app.config['MAIL_PORT'] = 587   ## 995
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_DEBUG'] = False
-app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = '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'
-app.config['MAIL_DEFAUL_SENDER'] = 'damiandrolas@gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465   ## 995
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') 
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+#app.config['MAIL_DEFAUL_SENDER'] = 
 app.config['MAIL_MAX_EMAILS'] = 5
 #app.config['MAIL_SUPPRESS_SEND']
 app.config['MAIL_ASCII_ATTACHMENT'] = False
@@ -35,7 +36,7 @@ def contacto():
   if form.validate_on_submit():
     flash('Mensaje enviado exitosamente', 'success')
     email_address = form.correo.data
-    texto_del_mensaje = form.message.data + form.correo.data
+    texto_del_mensaje = form.message.data
 
     # message = texto_del_mensaje
     # subject = email_address
@@ -44,7 +45,7 @@ def contacto():
     # server.login('damiandrolas@gmail.com','21509329')
     # server.sendmail('damiandrolas@gmail.com')
 
-    msg = Message( recipients=['damiandrolas@gmail.com'], body= texto_del_mensaje)
+    msg = Message(email_address, sender=email_address, recipients=['damiandrolas@gmail.com'], body= texto_del_mensaje)
     correo.send(msg)
     return redirect(url_for('home'))
 
@@ -57,7 +58,7 @@ def pricing():
 
 
 if __name__ == '__main__':
-    app.run()         ## mantiene abierta la app para escuchar  debug=True modo de prueba
+    app.run(debug = True)         ## mantiene abierta la app para escuchar  debug=True modo de prueba
 
 
 
